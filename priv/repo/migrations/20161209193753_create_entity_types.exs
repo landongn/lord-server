27c1e20e258928c0f5154fe476a85579685d4138
Landon Noss <landongn@gmail.com>
2016-12-09 16:37:27 -0800

    typo

--- priv/repo/migrations/20161209193753_create_entity_types.exs
+++ priv/repo/migrations/20161209193753_create_entity_types.exs
@@ -9,7 +9,7 @@ defmodule Server.Repo.Migrations.CreateEntityTypes do
       add :description, :string, default: nil
       add :player_kills_to_date, :integer, default: 0
       add :player_murders_to_date, :integer, default: 0
-      add :gold_invested_into_econ :integer, default: 0
+      add :gold_invested_into_econ, :integer, default: 0
 
       timestamps()
     end
