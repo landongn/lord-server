a9861ee34f97f674a93cabaf35e48259388a8948
landongn <landongn@gmail.com>
2016-12-12 22:11:11 -0800

    fixed some bugs with sounds and character data formatting" git push

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -70,8 +70,8 @@ Repo.insert!(%Server.Entity{
     m_health: 25,
     m_mana: 25,
     gold: 25,
-    kill_msg: "<li>You begin fumbling to keep hold of your weapon before it falls and hits the gnome and impales itself in his skull.</li><li>As you lean in to remove your weapon, a foul stench erupts from the other end of him.  It's probably wise to move on, quickly.</li>",
-    death_msg: "<li>\"What a moron\", the Gnome blurts as he draws his blade from your gushing throat.</li>",
+    kill_msg: "You begin fumbling to keep hold of your weapon before it falls and hits the gnome and impales itself in his skull.As you lean in to remove your weapon, a foul stench erupts from the other end of him.  It's probably wise to move on, quickly.",
+    death_msg: "\"What a moron\", the Gnome blurts as he draws his blade from your gushing throat.",
     weapon: "Makeshift Shank"
 })
 
@@ -90,8 +90,8 @@ Repo.insert!(%Server.Entity{
     s_hit: "skel_hit",
     s_atk: "skelatt1",
     s_die: "skel_die",
-    death_msg: "<li>The final blow knocks the entire ribcage of the skeleton tumbling off into the distance as it collapses into a pile of dusty bones.</li>",
-    kill_msg: "<li>\"Yeehehehehe\", the skeleton laughs as it begins ripping your corpse to shreds. After a few moments, there's nothing left but your weapon, and your gold.</li>",
+    death_msg: "The final blow knocks the entire ribcage of the skeleton tumbling off into the distance as it collapses into a pile of dusty bones.",
+    kill_msg: "\"Yeehehehehe\", the skeleton laughs as it begins ripping your corpse to shreds. After a few moments, there's nothing left but your weapon, and your gold.",
     weapon: "Rusty Long Sword"
 })
 
@@ -110,8 +110,8 @@ Repo.insert!(%Server.Entity{
     s_hit: "bat_hit",
     s_atk: "bat_att",
     s_die: "bat_die",
-    death_msg: "<li>The bat sinks its fangs into your jugular, popping open the artery. A fountain of blood flows from the wound as you slowly writhe on the ground, gasping your final breaths.</li>",
-    kill_msg: "<li>You snag the left wing of the bat with your right hand and yank the wing clean off. The creature flops around on the ground, in shock. You lean over it and crush its pathetic skull with your boot. </li>",
+    death_msg: "The bat sinks its fangs into your jugular, popping open the artery. A fountain of blood flows from the wound as you slowly writhe on the ground, gasping your final breaths.",
+    kill_msg: "You snag the left wing of the bat with your right hand and yank the wing clean off. The creature flops around on the ground, in shock. You lean over it and crush its pathetic skull with your boot. ",
     weapon: "Fangs"
 })
 
@@ -127,8 +127,8 @@ Repo.insert!(%Server.Entity{
     m_health: 25,
     experience: 8,
     m_mana: 25,
-    death_msg: "<li>As you lean in to strike, the old man clutches his chest and shouts \"Bedalia!\", before collapsing into a heap.</li>",
-    kill_msg: "<li>\"Are you KIDDING me?\", you exclaim, moments before the man's Cane caves your skull in.  The Old Man seems to have forgotten what was happening.</li>",
+    death_msg: "As you lean in to strike, the old man clutches his chest and shouts \"Bedalia!\", before collapsing into a heap.",
+    kill_msg: "\"Are you KIDDING me?\", you exclaim, moments before the man's Cane caves your skull in.  The Old Man seems to have forgotten what was happening.",
     s_hit: "gethit2ml",
     s_atk: "",
     s_die: "death_ml",
@@ -148,8 +148,8 @@ Repo.insert!(%Server.Entity{
     armor_value: 1,
     damage: 1,
     gold: 150,
-    kill_msg: "<li>\"Holy shit!\", an incredulous Bran stares at you, mouth agape. \"I did NOT think that would work!\".  You quietly die as Bran appears to be giving himself a high five.  How embarassing.</li>",
-    death_msg: "<li>As you land a vicious blow to Bran's arm, he screams out \"You cheating bastard!\", and runs away.  He appears to have dropped his gold, which you nab before moving on, confused.</li>",
+    kill_msg: "\"Holy shit!\", an incredulous Bran stares at you, mouth agape. \"I did NOT think that would work!\".  You quietly die as Bran appears to be giving himself a high five.  How embarassing.",
+    death_msg: "As you land a vicious blow to Bran's arm, he screams out \"You cheating bastard!\", and runs away.  He appears to have dropped his gold, which you nab before moving on, confused.",
     s_hit: "gethit3mb",
     s_die: "death_mb",
     weapon: "Short Sword"
@@ -170,8 +170,8 @@ Repo.insert!(%Server.Entity{
     experience: 10,
     m_health: 25,
     m_mana: 25,
-    kill_msg: "<li>In an instant, the tiny spider seems to disappear.  As you look around for that tiny rat bastard, you realize it, along with a half dozen other spiders, have crawled onto your back and injected you with toxins.  You messily expire as several other larger spiders head toward the kill.</li>",
-    death_msg: "<li>A decisive crunch reverberates as you crush the spider with the palm of your gauntlet. Gross.</li>",
+    kill_msg: "In an instant, the tiny spider seems to disappear.  As you look around for that tiny rat bastard, you realize it, along with a half dozen other spiders, have crawled onto your back and injected you with toxins.  You messily expire as several other larger spiders head toward the kill.",
+    death_msg: "A decisive crunch reverberates as you crush the spider with the palm of your gauntlet. Gross.",
     gold: 25,
     weapon: "Tiny Venomous Fangs"
 })
@@ -192,8 +192,8 @@ Repo.insert!(%Server.Entity{
     damage: 1,
     experience: 10,
     m_health: 25,
-    kill_msg: "<li>Somehow, a beetle the size of a football managed to kill a warrior.  A tiny, pathetic beetle. </li>",
-    death_msg: "<li>you grunt as your blade rips through the chitin of the beetle, severing it in twain. It squeals in stereo. </li>",
+    kill_msg: "Somehow, a beetle the size of a football managed to kill a warrior.  A tiny, pathetic beetle. ",
+    death_msg: "you grunt as your blade rips through the chitin of the beetle, severing it in twain. It squeals in stereo. ",
     m_mana: 25,
     gold: 25,
     weapon: "Glowing Mandible"
@@ -204,18 +204,18 @@ Repo.insert!(%Server.Entity{
     level: 1,
     strength: 1,
     endurance: 10,
-    s_atk: "beetlatt",
-    s_die: "beetldie",
-    s_hit: "beetlhit",
-    s_miss: "beetlwlk",
+    s_atk: "rat_att",
+    s_die: "rat_die",
+    s_hit: "rat_hit",
+    s_miss: "rat_idl",
     health: 25,
     armor_value: 1,
     damage: 1,
     experience: 10,
     m_health: 25,
     m_mana: 25,
-    kill_msg: "<li>A confused rat stares at you as an adult person simply falls over backwards.  When you hit the ground, you somehow manage to impale yourself on a nearby post. Great job.</li>",
-    death_msg: "<li>The rat squeals as you stab it through the skull.  You're one sick puppy. Maybe it's time to find something to kill that can defend itself? </li>",
+    kill_msg: "A confused rat stares at you as an adult person simply falls over backwards.  When you hit the ground, you somehow manage to impale yourself on a nearby post. Great job.",
+    death_msg: "The rat squeals as you stab it through the skull.  You're one sick puppy. Maybe it's time to find something to kill that can defend itself? ",
     gold: 25,
     weapon: "Filthy Teeth"
 })
@@ -234,8 +234,8 @@ Repo.insert!(%Server.Entity{
     experience: 10,
     m_health: 45,
     m_mana: 25,
-    kill_msg: "<li>As the orc pulls the spear out of your chest, he exclaims, \"The Legionnares will be pleased with this kill!\". The beast begins to scream and howl, alerting other nearby orcs.</li>",
-    death_msg: "<li>\"The Legionnares will avenge my death!\", predicts the orc.  You bring your leg up to its hip to give you some leverage to pull it back out of the orcs chestplate when you stumble and bonk the orc in the forehead.</li>",
+    kill_msg: "As the orc pulls the spear out of your chest, he exclaims, \"The Legionnares will be pleased with this kill!\". The beast begins to scream and howl, alerting other nearby orcs.",
+    death_msg: "\"The Legionnares will avenge my death!\", predicts the orc.  You bring your leg up to its hip to give you some leverage to pull it back out of the orcs chestplate when you stumble and bonk the orc in the forehead.",
     gold: 50,
     weapon: "Makeshift Spear"
 })
