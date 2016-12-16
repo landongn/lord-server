8d193337ae9b7143785ae1c48d5f5d6620032969
Landon Noss <lnoss@gaikai.com>
2016-12-15 17:38:30 -0800

    fucking auth work you pile of garbage

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -1,6 +1,7 @@
 defmodule Server.Auth do
   import Plug.Conn
   import Phoenix.Controller
+
   alias Server.Router.Helpers
   alias Server.Repo
   require Logger
@@ -11,39 +12,36 @@ defmodule Server.Auth do
 
   def call(conn, repo) do
     player_id = get_session(conn, :player_id)
-    Logger.info "plug seeking logged in session for player: #{inspect player_id}"
-    if player = player_id && repo.get(Server.Player, player_id) do
-      put_current_player(conn, player)
-    else
-      assign(conn, :current_player, nil)
+    if player_id do
+      Logger.info "player is on plug.call is: #{player_id}"
+      player = player_id && repo.get(Server.Player, player_id)
+      Logger.info "Found user? #{inspect player}"
+      assign(conn, :player, player)
+      Logger.info "player set on connection assigns. #{inspect conn.assigns}"
+      token = Phoenix.Token.sign(Server.Endpoint, "token", player.email)
+      assign(conn, :token, token)
     end
+    conn
   end
 
   def login(conn, player) do
     Logger.info "logging in: #{inspect player.id}"
+    token = Phoenix.Token.sign(Server.Endpoint, "token", player.email)
     conn
-    |> assign(:current_player, player.id)
-    |> put_session(:player_id, player.id)
+    |> assign(:player, player)
+    |> put_session(:player_id, player)
+    |> assign(:token, token)
     |> configure_session(renew: true)
   end
 
-  defp put_current_player(conn, player) do
-    token = Phoenix.Token.sign(conn, "player_id", player.id)
-    Logger.info "signing token for user: #{token}"
-    conn
-    |> assign(:current_player, player.id)
-    |> assign(:socket_token, token)
-    |> put_session(:socket_token, token)
-
-  end
-
+ 
   def logout(conn) do
     configure_session(conn, drop: true)
   end
 
   def authenticate_user(conn, _opts) do
-    Logger.info "authenticate_user: #{conn.assigns.current_player}"
-    if conn.assigns.current_player do
+    Logger.info "authenticate_user: #{conn.assigns.current_user}"
+    if conn.assigns.current_user do
       conn
     else
       conn
--- web/controllers/index_controller.ex
+++ web/controllers/index_controller.ex
@@ -48,17 +48,21 @@ defmodule Server.IndexController do
 
       case Server.Repo.get_by(Player, email: player["email"]) do
         user ->
-            Logger.info "Found user! #{inspect user}"
+            Logger.info "\n\nFound user! #{inspect user}"
             Server.Auth.login(conn, user)
 
-            redirect conn, to: "/play"
+            # redirect conn, to: "/play"
+            conn |> put_flash(:info, "You have logged in, #{user.email}")
+            render conn, "login.html", %{changeset: Player.new_account(%Player{}, player)}
 
         {:error, _} ->
-            Logger.info "Could not find user, passed through."
+            conn |> put_flash(:error, "unable to log the user in, no record found")
+            Logger.info "\n\nCould not find user, passed through."
             conn.put_flash(:error, "unable to find an account. Sorry.")
             render conn, "login.html"
 
         nil ->
+          conn |> put_flash(:info, "unable to log the user in, no record found")
           render conn, "login.html"
       end
   end
